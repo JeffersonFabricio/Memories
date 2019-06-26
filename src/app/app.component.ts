084a4c0a0efbe82870d515b41bbb26810e084cef
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-06-26 16:46:14 -0300

    directives for PhotoModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  photos = [
+    {
+      url: 'https://img.ibxk.com.br///2019/04/01/01110608727063-t1200x480.jpg',
+      description: 'Você é o cara'
+    },
+    {
+      url: 'https://amp.businessinsider.com/images/5d0bae5ce3ecba536905f1c3-750-563.jpg',
+      description: 'Hmm'
+    },
+    {
+      url: 'https://cms.qz.com/wp-content/uploads/2018/11/elon-musk-tesla-paypal-credit-card.jpg?quality=75&strip=all&w=410&h=230.625',
+      description: 'Hmm2'
+    }
+
+    
+  ];
 }
