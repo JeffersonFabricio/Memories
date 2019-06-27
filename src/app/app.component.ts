a0a22df7af692e54e4556b8a363d6c77d458d995
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-06-27 11:11:12 -0300

    adding NotFoundComponent, PhotoFormComponent and adjust routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { PhotoService } from './photos/photo/photo.service';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  photos: any[] = [];
   
-  constructor(private photoService: PhotoService) { }
+  constructor() { }
   
   ngOnInit() {
-
-    this.photoService
-      .listFromUser('flavio')
-      .subscribe(photos => this.photos = photos);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-
+import { ErrorsModule } from './errors/errors.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
@@ -14,7 +13,8 @@ import { PhotosModule } from './photos/photos.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PhotosModule
+    PhotosModule,
+    ErrorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
