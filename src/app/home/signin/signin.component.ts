e280b26b1bd9eff87c6a03f0b31f89f49d1d996e
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-07-01 09:15:03 -0300

    adding validations in LoginForm

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html'
 })
-export class SignInComponent { }
+export class SignInComponent {
+
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.loginForm = this.fb.group({
+            userName: ['', Validators.required ],
+            password: ['', Validators.required ]
+        });
+    }
+
+}
