7d69216679ea9d27b4355741b47fe838773cc762
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-07-01 11:22:39 -0300

    Include validation in LoginForm

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { AuthService } from '../../core/auth.service';
+
 @Component({
     templateUrl: './signin.component.html'
 })
@@ -8,13 +10,32 @@ export class SignInComponent {
 
     loginForm: FormGroup;
 
-    constructor(private fb: FormBuilder) {}
+    constructor(
+        private fb: FormBuilder,
+        private as: AuthService
+    ) {}
 
     ngOnInit(): void {
         this.loginForm = this.fb.group({
             userName: ['', Validators.required ],
             password: ['', Validators.required ]
         });
+    }
+
+    login() {
+        const userName = this.loginForm.get('userName').value;
+        const password = this.loginForm.get('userName').value;
+
+        this.as
+            .authenticate(userName, password)
+            .subscribe(
+                () => console.log('autenticado'),
+                err => {
+                    console.log(err);
+                    this.loginForm.reset();
+                    alert('Usuário ou Senha inválida');
+                }
+            );
     }
 
 }
