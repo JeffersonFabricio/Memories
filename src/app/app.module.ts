ce66154f2afe0734d09aaeccb25c218bd75623d8
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-07-02 14:55:35 -0300

    create component Header for application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ErrorsModule } from './errors/errors.module';
     PhotosModule,
     AppRoutingModule,
     ErrorsModule,
-    HomeModule
+    HomeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
