3e10eb89462f26fa6d0e4a2697d9fe81873d2f38
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-07-02 15:56:21 -0300

    adding name logged in headerComponent

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,7 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { UserService } from '../user/user.service';
+import { User } from '../user/user';
+
 @Component ({
     selector: 'm-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    user$: Observable<User>;
+    user: User;
+
+    constructor(userService: UserService) {
+        this.user$ = userService.getUser();
+        this.user$.subscribe(user => this.user = user);
+    }
+}
