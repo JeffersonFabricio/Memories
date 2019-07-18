b1015aae20de643c89afd163dce0b1e6607dcd63
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-07-18 10:15:17 -0300

    create signUp component

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './core/auth/auth.guard';
 import { SignInComponent } from './home/signin/signin.component';
+import { SignUpComponent } from './home/signup/signup/signup.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
-import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
     { path: '', component: SignInComponent, canActivate: [AuthGuard] },
+    { path: 'signup', component: SignUpComponent },
     { path: 'user/:userName', component: PhotoListComponent, resolve: { photos: PhotoListResolver} },
     { path: 'p/add', component: PhotoFormComponent },
     { path: '**', component: NotFoundComponent }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { VMsgModule } from '../shared/components/vmsg/vmsg.module';
 import { SignInComponent } from './signin/signin.component';
+import { SignUpComponent } from './signup/signup/signup.component';
 
 @NgModule({
-    declarations: [ SignInComponent ],
+    declarations: [ SignInComponent, SignUpComponent ],
     imports: [
         CommonModule,
+        FormsModule,
         ReactiveFormsModule,
         VMsgModule,
         RouterModule
