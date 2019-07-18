0850a04f79da9e050820fa5c79b0ef0fe443754d
Jefferson Fabrício <jeffersonfabricio@tce.pe.gov.br>
2019-07-18 16:17:21 -0300

    validation in form signup

--- src/app/home/signup/signup/signup.component.ts
+++ src/app/home/signup/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'm-signup',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.signupForm = this.fb.group({
+      email: ['', 
+        [ 
+          Validators.required, 
+          Validators.email 
+        ] 
+      ],
+      fullName: ['', 
+        [
+          Validators.required, 
+          Validators.minLength(2),
+          Validators.maxLength(40)
+        ]
+      ],
+      userName: ['',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-z0-9_\-]+$/),
+          Validators.minLength(2),
+          Validators.maxLength(30)
+        ] 
+      ],
+      password: ['',
+        [
+          Validators.required, 
+          Validators.minLength(8),
+          Validators.maxLength(14)
+        ]
+      ]
+
+    })
   }
 
 }
